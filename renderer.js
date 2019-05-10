b6de308670e28eb808008a907b65d3a3d3d16ced
rgwch <weirich@elexis.ch>
2019-05-10 22:08:49 +0200

    backup set

--- renderer.js
+++ renderer.js
@@ -1,45 +1,34 @@
-const {exec,spawn} = require('child_process')
-const path=require('path')
+const { spawn } = require('child_process')
 const cfg = new (require('conf'))()
-
-let reponame = cfg.get("reponame") || "local"
-let repoaddress = (cfg.get("repoaddr") || "~/restic_repo").replace("~",require('os').homedir())
-let repopwd = cfg.get("repopwd") || "topsecret"
-let repodirs = cfg.get("repdirs") || __dirname
-
+const set = Object.assign({}, {
+	repoaddress: "~/restic_repo".replace("~", require('os').homedir()),
+	repopwd: "topsecret",
+	repodirs: __dirname
+}, cfg.get("default"))
 
 const setValues = () => {
 	let el = document.getElementById("disp_reponame")
-	el.textContent = reponame
+	el.textContent = set.repoaddress
 	el = document.getElementById("repourl")
-	el.value = repoaddress
+	el.value = set.repoaddress
 	el = document.getElementById('repopwd')
-	el.value = repopwd
+	el.value = set.repopwd
 	el = document.getElementById("disp_dirs")
-	el.textContent = repodirs
+	el.textContent = set.repodirs
 	el = document.getElementById('repodirs')
-	el.value = repodirs
+	el.value = set.repodirs
 }
 
 const getValues = () => {
 	let el = document.getElementById("disp_reponame")
-	reponame = el.textContent = reponame
+	set.reponame = el.textContent
 	el = document.getElementById("repourl")
-	el.value = repoaddress
+	set.repoaddress = el.value
 	el = document.getElementById('repopwd')
-	el.value = repopwd
-	el = document.getElementById("disp_dirs")
-	el.textContent = repodirs
+	set.repopwd = el.value
 	el = document.getElementById('repodirs')
-	el.value = repodirs
-}
-const callback = (err, stdout, stderr) => {
-	if (err) {
-		console.log(`exec error: ${err}`);
-		return;
-	} else {
-		console.log(`${stdout}`);
-	}
+	set.repodirs = el.value
+
 }
 
 function toggle(elem) {
@@ -51,41 +40,34 @@ function toggle(elem) {
 	}
 }
 
-function do_spawn(...cmd){
+function do_spawn(...cmd) {
 	const env = Object.assign({}, process.env)
-	env.RESTIC_PASSWORD = repopwd
-	const args=["-r",repoaddress,...cmd]
-	console.log(args)
-	const proc=spawn("restic",args,{env} )
-	proc.stdout.on('data',chunk=>{
-		console.log("stdout: "+chunk)
+	env.RESTIC_PASSWORD = set.repopwd
+	const args = ["-r", set.repoaddress, ...cmd]
+	const proc = spawn("restic", args, { env })
+	proc.stdout.on('data', chunk => {
+		console.log("stdout: " + chunk)
 	})
-	proc.stderr.on('data',err=>{
-		console.log("err: "+err)
+	proc.stderr.on('data', err => {
+		console.log("err: " + err)
 	})
-	proc.on("close",exitcode=>{
-		console.log("exit: "+exitcode)
+	proc.on("close", exitcode => {
+		console.log("exit: " + exitcode)
 	})
 }
-function init(repo, pwd) {
-	const r = repo || repoaddress
-	const p = pwd || repopwd
-	const env = Object.assign({}, process.env)
-	env.RESTIC_PASSWORD = p
-	exec(`restic -r ${r} init`, { env }, callback);
+function init() {
+	do_spawn("init")
 }
 
-function list(){
+function list() {
 	do_spawn("snapshots")
 }
 function backup() {
-	do_spawn("backup",repodirs)
+	do_spawn("backup", set.repodirs)
 }
 
 function restore() {
-	const env = Object.assign({}, process.env)
-	env.RESTIC_PASSWORD = pwdlementById("searchTxt").value;
-	exec(`restic -r ${repo} restore latest --target ${dirs}`, { env }, callback);
+	do_spawn("xrestore", "latest", "--target", set.repodirs)
 }
 
 setValues()
