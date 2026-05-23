65445eb478f6309ac4a3722795091cdc31913163
rgwch <rgw@rgw.ch>
2026-05-23 08:09:15 +0200

    remove recreatable files

--- build/server/chunks/2-Cq-XANe6.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-DzAFgZU4.js')).default;
-const imports = ["_app/immutable/nodes/2.CqiazCxP.js","_app/immutable/chunks/Cuvso2Uc.js","_app/immutable/chunks/CM4ER8Vs.js","_app/immutable/chunks/BtOvOIpC.js","_app/immutable/chunks/BpCV0uRN.js","_app/immutable/chunks/DPNdIkwL.js","_app/immutable/chunks/D3Z-hH9B.js","_app/immutable/chunks/BZYOG5L8.js"];
-const stylesheets = ["_app/immutable/assets/2.Rbk5g5QV.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-Cq-XANe6.js.map
