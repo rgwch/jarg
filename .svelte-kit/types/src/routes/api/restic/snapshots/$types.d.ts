eb103b735202f128564a2b0d200a9f45d2d41aac
rgwch <rgw@rgw.ch>
2026-05-23 08:13:51 +0200

    ignore .svelte-kit

--- .svelte-kit/types/src/routes/api/restic/snapshots/$types.d.ts
+++
@@ -1,9 +0,0 @@
-import type * as Kit from '@sveltejs/kit';
-
-type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-type MatcherParam<M> = M extends (param : string) => param is (infer U extends string) ? U : string;
-type RouteParams = {  };
-type RouteId = '/api/restic/snapshots';
-
-export type RequestHandler = Kit.RequestHandler<RouteParams, RouteId>;
-export type RequestEvent = Kit.RequestEvent<RouteParams, RouteId>;
