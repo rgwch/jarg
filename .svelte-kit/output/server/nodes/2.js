10e7dc446d6d9d634bb174f2d78d2aad24cb78ef
rgwch <rgw@rgw.ch>
2026-05-23 08:09:38 +0200

    remove recreatable files

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CqiazCxP.js","_app/immutable/chunks/Cuvso2Uc.js","_app/immutable/chunks/CM4ER8Vs.js","_app/immutable/chunks/BtOvOIpC.js","_app/immutable/chunks/BpCV0uRN.js","_app/immutable/chunks/DPNdIkwL.js","_app/immutable/chunks/D3Z-hH9B.js","_app/immutable/chunks/BZYOG5L8.js"];
-export const stylesheets = ["_app/immutable/assets/2.Rbk5g5QV.css"];
-export const fonts = [];
